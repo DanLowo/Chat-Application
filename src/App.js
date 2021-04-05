4305669df7fc8c2d71c2d2166dde9632858bc02d
DanLowo <danlowo@horbify.com>
2021-04-05 19:12:45 +0100

    done navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
+import "./css/Hiring_Design/App.css"
+import HiringDesign from './Projects/Hiring_Design/index'
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <HiringDesign/>
+  </div>;
 }
 
 export default App;
---
+++ src/Projects/Hiring_Design/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Hiring_Design/navbar'
+
+function index() {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default index
