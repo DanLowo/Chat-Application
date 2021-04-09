530ccc206e924e55caeb7f328742d9b9cb3f8902
DanLowo <danlowo@horbify.com>
2021-04-09 14:29:15 +0100

    done cart page

--- src/Projects/fruit/cart.js
+++ src/Projects/fruit/cart.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import "./assets/App.css"
+
+import Typography from "@material-ui/core/Typography"
+
+import BottomNav from "./components/BottomNav"
+import CartGrid from './components/cartGrid'
+
 function Cart() {
     return (
-        <div>
-            
+        <div id="fruit">
+            <Typography align="center" id="cartTitle">My Cart</Typography>
+            <CartGrid/>
+            <div id="bottomNavSpacing">
+                <BottomNav />
+            </div>
+
         </div>
     )
 }
--- src/Projects/fruit/viewFruit.js
+++ src/Projects/fruit/viewFruit.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import "./assets/App.css"
+
 // import all components for view fruit
 import NavBar from "./components/viewFruit/navbar"
 import Details from "./components/viewFruit/details"
