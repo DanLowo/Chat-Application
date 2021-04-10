ec651d95507088d9ff6d3fd80bb3ddcf3e8298e1
DanLowo <danlowo@horbify.com>
2021-04-10 18:29:02 +0100

    done with subscriptions component

--- src/Projects/fintech/components/Subscriptions.js
+++ src/Projects/fintech/components/Subscriptions.js
@@ -1,6 +1,17 @@
 import React, { useState } from 'react'
 
 import Typography from "@material-ui/core/Typography"
+import Grid from "@material-ui/core/Grid"
+import Divider from "@material-ui/core/Divider"
+import Paper from "@material-ui/core/Paper"
+
+// import imagese
+import Netflix from "../assets/netflix.jpeg"
+import Spotify from "../assets/spotify.png"
+import Medium from "../assets/medium.png"
+import Adobe from "../assets/adobe.png"
+import Dstv from "../assets/dstv.jpg"
+import LinkedIn from "../assets/linkedin.webp"
 
 function Subscriptions() {
 
@@ -11,14 +22,72 @@ function Subscriptions() {
     }
     const [activeTab, setActiveTab] = useState(ok)
 
+    const allSubscription = [
+        {
+            name: 'Netflix',
+            price: '4,400',
+            due: 'Today',
+            type: 'Monthly',
+            img: Netflix
+        },
+        {
+            name: 'Medium',
+            price: '5,600',
+            due: '12/03',
+            type: 'Bi-Weekly',
+            img: Medium
+        },
+        {
+            name: 'LinkedIn',
+            price: '10,000',
+            due: '14/04',
+            type: 'Yearly',
+            img: LinkedIn
+        },
+        {
+            name: 'DSTV',
+            price: '2,000',
+            due: '1/03',
+            type: 'Monthly',
+            img: Dstv
+        },
+    ]
+
+    const UpcomingSubscription = [
+        {
+            name: 'Spotify',
+            price: '10,000',
+            due: '14/04',
+            type: 'Yearly',
+            img: Spotify
+        },
+        {
+            name: 'DSTV',
+            price: '2,000',
+            due: '1/03',
+            type: 'Monthly',
+            img: Dstv
+        }
+    ]
+
+    const PendingSubscription = [
+        {
+            name: 'Adobe Lightroom',
+            price: '4,000',
+            due: '14/04',
+            type: 'Yearly',
+            img: Adobe
+        }
+    ]
+
     const handleSetTab = (tabType) => {
-        switch(tabType){
+        switch (tabType) {
             case 'All':
                 return setActiveTab(ok);
             case 'Upcoming':
-                return setActiveTab({All: false, Upcoming: true, Pending: false});
+                return setActiveTab({ All: false, Upcoming: true, Pending: false });
             case 'Pending':
-                return setActiveTab({All: false, Upcoming: false, Pending: true});
+                return setActiveTab({ All: false, Upcoming: false, Pending: true });
             default:
                 return setActiveTab(ok);
         }
@@ -27,16 +96,88 @@ function Subscriptions() {
     return (
         <div id="subscriptions">
             <Typography id="mySubscriptoins">My Subscriptions</Typography>
+
             <div id="subscriptionTabs">
                 <div id="subscriptionTab" className={activeTab.All ? 'activeTab' : ''} align="center" onClick={() => handleSetTab('All')}>
-                    <span className={activeTab.All ? 'activeTabItem' : ''} id="subscriptionTabItem" align="center">All</span> 
+                    <span className={activeTab.All ? 'activeTabItem' : ''} id="subscriptionTabItem" align="center">All</span>
                 </div>
                 <div id="subscriptionTab" className={activeTab.Upcoming ? 'activeTab' : ''} align="center" onClick={() => handleSetTab('Upcoming')}>
-                    <span className={activeTab.Upcoming ? 'activeTabItem' : ''} id="subscriptionTabItem">Upcoming</span> 
+                    <span className={activeTab.Upcoming ? 'activeTabItem' : ''} id="subscriptionTabItem">Upcoming</span>
                 </div>
                 <div id="subscriptionTabLast" className={activeTab.Pending ? 'activeTab' : ''} align="center" onClick={() => handleSetTab('Pending')}>
-                    <span className={activeTab.Pending ? 'activeTabItem' : ''} id="subscriptionTabItem">Pending</span> 
+                    <span className={activeTab.Pending ? 'activeTabItem' : ''} id="subscriptionTabItem">Pending</span>
                 </div>
+            </div>
+
+            <div id="subscriptionPlans">
+                {activeTab.All &&
+                    allSubscription.map(subscription => (
+                        <div key={subscription.name}>
+                            <Grid container id="subscriptionPlansItem">
+                                <Grid item xs={2} sm={2} md={2}>
+                                    <Paper id="paper">
+                                        <img alt={subscription.name} src={subscription.img} />
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={7} sm={7} md={7} id="details">
+                                    <Typography id="name"> {subscription.name} </Typography>
+                                    <Typography id="type"> {subscription.type} </Typography>
+                                </Grid>
+                                <Grid item xs={3} sm={3} md={3} id="price">
+                                    <Typography id="name"> N {subscription.price} </Typography>
+                                    <Typography className={subscription.due === 'Today' ? 'dueToday' : ''} id="due"> Due: {subscription.due} </Typography>
+                                </Grid>
+                            </Grid>
+                            <Divider />
+                        </div>
+                    ))
+                }
+
+                {activeTab.Upcoming &&
+                    UpcomingSubscription.map(subscription => (
+                        <div key={subscription.name}>
+                            <Grid container id="subscriptionPlansItem">
+                                <Grid item xs={2} sm={2} md={2}>
+                                    <Paper id="paper">
+                                        <img alt={subscription.name} src={subscription.img} />
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={7} sm={7} md={7} id="details">
+                                    <Typography id="name"> {subscription.name} </Typography>
+                                    <Typography id="type"> {subscription.type} </Typography>
+                                </Grid>
+                                <Grid item xs={3} sm={3} md={3} id="price">
+                                    <Typography id="name"> N {subscription.price} </Typography>
+                                    <Typography className={subscription.due === 'Today' ? 'dueToday' : ''} id="due"> Due: {subscription.due} </Typography>
+                                </Grid>
+                            </Grid>
+                            <Divider />
+                        </div>
+                    ))
+                }
+
+                {activeTab.Pending &&
+                    PendingSubscription.map(subscription => (
+                        <div key={subscription.name}>
+                            <Grid container id="subscriptionPlansItem">
+                                <Grid item xs={2} sm={2} md={2}>
+                                    <Paper id="paper">
+                                        <img alt={subscription.name} src={subscription.img} />
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={7} sm={7} md={7} id="details">
+                                    <Typography id="name"> {subscription.name} </Typography>
+                                    <Typography id="type"> {subscription.type} </Typography>
+                                </Grid>
+                                <Grid item xs={3} sm={3} md={3} id="price">
+                                    <Typography id="name"> N {subscription.price} </Typography>
+                                    <Typography className={subscription.due === 'Today' ? 'dueToday' : ''} id="due"> Due: {subscription.due} </Typography>
+                                </Grid>
+                            </Grid>
+                            <Divider />
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
