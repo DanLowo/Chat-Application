43d66c400312ea68319f86a4932be24b36322eef
DanLowo <danlowo@horbify.com>
2021-04-11 16:29:48 +0100

    finished submit card navigation drawer

--- src/Projects/fintech/newCard.js
+++ src/Projects/fintech/newCard.js
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 
 import "./assets/App.css"
 
+import {Link} from "react-router-dom"
+
 // import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
 import InputLabel from "@material-ui/core/InputLabel"
 import Grid from "@material-ui/core/Grid"
 import Button from "@material-ui/core/Button"
+import Drawer from "@material-ui/core/Drawer"
+import Typography from "@material-ui/core/Typography"
 
 import Card from "./components/card"
 import BottomNav from "./components/bottomNav"
 import DynamicBar from './components/dynamicBar'
+
+import CardInWallet from "./assets/card-in-wallet.jpg"
 
 function NewCard() {
 
@@ -18,13 +24,19 @@ function NewCard() {
     const [cardNumber, setCardNumber] = useState('')
     const [cardExpiryDate, setCardExpiryDate] = useState('')
     const [cardCVV, setCardCVV] = useState('')
+    const [navigation, setNavigation] = useState(false)
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        setNavigation(true)
+    }
 
     return (
         <div>
             <div id="fintech">
                 <DynamicBar title="Add a Card" />
                 <Card name={cardName} expiryDate={cardExpiryDate} number={cardNumber} />
-                <form id="addCardForm">
+                <form id="addCardForm" onSubmit={handleSubmit}>
                     <InputLabel id="nameOnCard">Name on card</InputLabel>
                     <TextField required type="text" value={cardName} onChange={(e) => setCardName(e.target.value)} size="medium" fullWidth variant="outlined"></TextField>
                     <br /> <br />
@@ -69,6 +81,14 @@ function NewCard() {
                     <Button type="submit" fullWidth variant="contained" color="primary" id="noSubscriptionsButton">Add Card</Button>
                 </form>
             </div>
+            <Drawer align="center" anchor="bottom" open={navigation} onClose={() => setNavigation(false)}>
+                <div id="addCardDrawer">
+                    <img alt="card" src={CardInWallet}  id="addedCardImg" />
+                    <Typography variant="h6">Bank Card Added</Typography>
+                    <Typography variant="body2">You have successfully added a new bank card to your account</Typography>
+                    <Button component={Link} to="/fintech" fullWidth variant="contained" color="primary" id="noSubscriptionsButton">Done</Button>
+                </div>
+            </Drawer>
             <div id="bottomNavSpacing">
                 <BottomNav />
             </div>
