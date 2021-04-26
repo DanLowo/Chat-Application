e1444679fc11fd0e177a2537b4f22fb3b3c1f238
Daniel <don@Don.Don>
2021-04-26 11:38:59 +0100

    removed all project from master branch

--- src/App.js
+++ src/App.js
@@ -1,74 +1,13 @@
 
-// import all pages for hiring design
-import HiringDesign from './Projects/hiring/index'
-import ViewJob from './Projects/hiring/viewJob'
-
-
-// import all pages for fruit design
-import FruitDesign from "./Projects/fruit/index"
-import ViewFruit from "./Projects/fruit/viewFruit"
-import Cart from "./Projects/fruit/cart"
-import Account from "./Projects/fruit/account" 
-
-
-// import all pages from fintech design
-import FintechDesign from "./Projects/fintech/index"
-import Subscriptions from "./Projects/fintech/subscriptions"
-import Notifications from "./Projects/fintech/notifications"
-import Cards from "./Projects/fintech/cards"
-import NewCard from "./Projects/fintech/newCard"
-import Profile from "./Projects/fintech/profile"
-
-
 import { Switch, Route } from "react-router-dom"
 
 function App() {
   return <div className="App">
       <Switch>
-
-      {/* set all hirign design routes */}
-        <Route exact path="/">
-          <HiringDesign/>
-        </Route>
-        <Route path="/view-job">
-          <ViewJob/>
-        </Route>
-
-
-      {/* set all fruit design routes */}
-        <Route path="/fruit">
-          <FruitDesign/>
-        </Route>
-        <Route path="/fruits/view-fruit">
-          <ViewFruit/>
-        </Route>
-        <Route path="/fruits/cart">
-          <Cart/>
-        </Route>
-        <Route path="/fruits/account">
-          <Account/>
-        </Route>
-
-
         {/* set all fintech design routes */}
-          <Route exact path="/fintech">
+          {/* <Route exact path="/fintech">
               <FintechDesign/>
-          </Route>
-          <Route path="/fintech/subscriptions">
-              <Subscriptions/>
-          </Route>
-          <Route path="/fintech/notifications">
-              <Notifications/>
-          </Route>
-          <Route path="/fintech/cards">
-              <Cards/>
-          </Route>
-          <Route path="/fintech/new-card">
-              <NewCard/>
-          </Route>
-          <Route path="/fintech/profile">
-              <Profile/>
-          </Route>
+          </Route> */}
 
       </Switch>
   </div>;
--- src/Projects/fintech/assets/Fintech/README.md
+++
@@ -1,3 +0,0 @@
-### UI DESIGN
-
-This are the UI desgins created by Onyinye Chukwuka (https://www.linkedin.com/in/ACoAACW4ziIBrajtoAK2qMr--7bV0NCgL6yal64?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recent_activity_details_shares%3BmRAfjABOTcyDMywkNZo7Nw%3D%3D)
